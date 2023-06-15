fccebda39e96ef86dab0c761d0063cf7b624e958
Raghuvir Singh <raghuvir9923@gmail.com>
2023-06-15 15:22:37 +0530

    navbar

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -121,14 +121,34 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 console.log('Hello, Parcel!');
 window.addEventListener('scroll', function () {
   var navbar = document.querySelector('.navbar');
-  var logo = document.querySelector('.nav-links');
+  var logo = document.querySelector('.nav-links-inner');
   if (window.scrollY > 0) {
     navbar.classList.add('sticky');
-    logo.style.opacity = '0';
+    logo.classList.add('fade');
   } else {
     navbar.classList.remove('sticky');
-    logo.style.opacity = '1';
+    logo.classList.remove('fade');
   }
+  if (window.scrollY === 0) {
+    logo.classList.remove('fade');
+  }
+});
+window.addEventListener('scroll', function () {
+  var navbar = document.querySelector('.navbar');
+  var logo = document.querySelector('.logo-text');
+  if (window.scrollY > 0) {
+    navbar.classList.add('sticky');
+    logo.classList.add('fade');
+  } else {
+    navbar.classList.remove('sticky');
+    logo.classList.remove('fade');
+  }
+});
+var rainbowButton = document.querySelector('.rainbow-bg');
+document.addEventListener('mousemove', function (e) {
+  var x = e.clientX / window.innerWidth;
+  var y = e.clientY / window.innerHeight;
+  rainbowButton.style.backgroundPosition = "".concat(x * 200, "% ").concat(y * 200, "%");
 });
 },{}],"../../../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -155,7 +175,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61988" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50598" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- src/script.js
+++ src/script.js
@@ -1,14 +1,39 @@
 console.log('Hello, Parcel!');
 window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    const logo = document.querySelector('.nav-links');
-  
-    if (window.scrollY > 0) {
-      navbar.classList.add('sticky');
-      logo.style.opacity = '0';
-    } else {
-      navbar.classList.remove('sticky');
-      logo.style.opacity = '1';
-    }
-  });
-  
+  const navbar = document.querySelector('.navbar');
+  const logo = document.querySelector('.nav-links-inner');
+
+  if (window.scrollY > 0) {
+    navbar.classList.add('sticky');
+    logo.classList.add('fade');
+  } else {
+    navbar.classList.remove('sticky');
+    logo.classList.remove('fade');
+  }
+  if (window.scrollY === 0) {
+    logo.classList.remove('fade');
+  }
+});
+
+
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
+  const logo = document.querySelector('.logo-text');
+
+  if (window.scrollY > 0) {
+    navbar.classList.add('sticky');
+    logo.classList.add('fade');
+  } else {
+    navbar.classList.remove('sticky');
+    logo.classList.remove('fade');
+  }
+});
+
+const rainbowButton = document.querySelector('.rainbow-bg');
+
+document.addEventListener('mousemove', (e) => {
+  const x = e.clientX / window.innerWidth;
+  const y = e.clientY / window.innerHeight;
+
+  rainbowButton.style.backgroundPosition = `${x * 200}% ${y * 200}%`;
+});
